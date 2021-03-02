59b41efa5cbb5a2b5f516155607fcc65703f7398
andriansebayang <56537341+andriansebayang@users.noreply.github.com>
2021-03-02 11:28:18 +0700

    Create input.cpp
    Input

---
+++ input.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int angka1;
+int angka2; 
+
+cout << "Bilangan 1 = ";
+cin>>angka1;
+cout << "Bilangan 2 = " ;
+cin>>angka2;
+
+cout << "Bilangan 1 =" << angka1<<endl;
+cout << "Bilangan 2 =" << angka2<<endl;
+
+return 0;
+}

4eb57287686dfc2404b2a67808954edc8df3d617
andriansebayang <56537341+andriansebayang@users.noreply.github.com>
2021-03-02 11:27:06 +0700

    Create kali.cpp
    Kali

---
+++ kali.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+int angka1 =10;
+int angka2 = 2; 
+int hasil;
+cout << "Bilangan 1 = "<<angka1<<endl ;
+
+cout << "Bilangan 2 = "<<angka2<<endl ;
+
+hasil = angka1 * angka2;
+cout << "Hasil  Kali  = " << hasil<<endl;
+
+return 0;
+}
